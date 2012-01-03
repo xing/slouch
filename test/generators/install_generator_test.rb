4c44eae1193cca44af137943914717f15ba469d1
Tim Payton <timpayton@gmail.com>
2012-01-03 09:47:44 +0100

    Extreme refactoring ;)

--- test/generators/install_generator_test.rb
+++ test/generators/install_generator_test.rb
@@ -1,40 +1,42 @@
 require "test_helper"
 
 class InstallGeneratorTest < ::Rails::Generators::TestCase
+  TMP_DIR = "test/tmp"
+  APP_JS_REL_PATH = "app/assets/javascripts/application.js"
+
   tests ::Backbone::InstallGenerator
-  destination File.expand_path("../../tmp", __FILE__)
+  destination TMP_DIR
 
   setup :prepare_destination
+  setup :create_application_js
+
+  teardown :destroy_tmp_dir
 
   test "install generator insert require underscore into application.js" do
     run_generator
-    assert_file "app/assets/javascripts/application.js", /\/\/= require\sunderscore/
+    assert_file APP_JS_REL_PATH, /\/\/= require\sunderscore/
   end
 
   test "install generator inserts require backbone into application.js" do
     run_generator
-    assert_file "app/assets/javascripts/application.js", /\/\/= require\sbackbone/
+    assert_file APP_JS_REL_PATH, /\/\/= require\sbackbone/
   end
 
   test "install generator injects backbone dependencies in the right order: first underscore then backbone" do
     run_generator
-    assert_file "app/assets/javascripts/application.js", /\/\/= require\sunderscore\n\/\/= require\sbackbone/
+    assert_file APP_JS_REL_PATH, /\/\/= require\sunderscore\n\/\/= require\sbackbone/
   end
 
   private
 
   def create_application_js
-    tmp_dir = File.expand_path("../../tmp", __FILE__)
-
-    FileUtils.mkdir_p "#{tmp_dir}/app/assets/javascripts/"
-    FileUtils.cp "test/fixtures/application.js", "#{tmp_dir}/app/assets/javascripts/application.js"
+    FileUtils.mkdir_p "#{TMP_DIR}/app/assets/javascripts/"
+    FileUtils.cp "test/fixtures/application.js", "#{TMP_DIR}/#{APP_JS_REL_PATH}"
   end
-  setup :create_application_js
 
-  def destroy_application_js
-    FileUtils.rm_rf "test/tmp"
+  def destroy_tmp_dir
+    FileUtils.rm_rf TMP_DIR
   end
-  teardown :destroy_application_js
 
 end
 
