16b8c05217f3f561c95cb539a7d75d832664b168
Oemuer Oezkir <oemuer.oezkir@xing.com>
2012-01-05 11:39:41 +0100

    Added a skeleton router template

--- lib/generators/slouch/router_generator.rb
+++ lib/generators/slouch/router_generator.rb
@@ -1,8 +1,11 @@
 require "rails/generators"
+require "generators/slouch/helper"
 
 module Slouch
   module Generators
     class RouterGenerator < ::Rails::Generators::Base
+      include ::Slouch::Generators::Helper
+
       source_root File.expand_path('../templates', __FILE__)
 
       def create
---
+++ test/router_generator_test.rb
@@ -0,0 +1,32 @@
+require "test_helper"
+require "generators/slouch/router_generator"
+
+class RouterGeneratorTest < ::Rails::Generators::TestCase
+  tests ::Slouch::Generators::RouterGenerator
+
+  MODEL_NAME = "ProductLine"
+  destination TMP_DIR
+
+  setup    :prepare_destination
+  teardown :destroy_tmp_dir
+
+  test "should create a router in the app namespace object" do
+    run_generator
+    assert_file "app/assets/javascripts/routers/router.js",
+                /#{application_name} = \(\s*function/
+  end
+
+  test "should create a router in the Routers namespace" do
+    run_generator
+    assert_file "app/assets/javascripts/routers/router.js",
+                /public_\.Routers\s+=/
+  end
+
+  test "should create a skeleton router with routes and initialize properties" do
+    run_generator
+    assert_file "app/assets/javascripts/routers/router.js",
+                /routes:\s{/
+    assert_file "app/assets/javascripts/routers/router.js",
+                /initialize:\sfunction\(\)\s{/
+  end
+end
